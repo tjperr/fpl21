c716e274ff6c437d702216b16de265b051eaf0bc
tjperr <thomas_perrett@outlook.com>
2021-08-22 12:01:43 +0100

    report best

--- fpl21/game.py
+++ fpl21/game.py
@@ -10,6 +10,9 @@ def player_score(pid):
     # lets see if MC can find this player!
     if pid == 252:
         return 10
+    # and favour even ids
+    if pid % 2 == 0:
+        return random.choice(range(11))    
     return random.choice(range(10))
 
 
@@ -17,7 +20,7 @@ my_squad = Squad([30, 80, 275, 198, 110, 252, 62, 22, 35, 196, 277, 359, 413, 33
 score_dict = {pid: [] for pid in my_squad.players}
 
 # MC for team selection
-for _ in range(10000):
+for _ in range(1000):
     team = (
         random.sample(my_squad.goalkeepers, 1)
         + random.sample(my_squad.defenders, 3)
@@ -31,6 +34,15 @@ for _ in range(10000):
     for pid in my_squad.selection:
         score_dict[pid].append(score)
     
+summary_score_dict = {}
 for pid, scores in score_dict.items():
-    print(f"{pid:4}: {np.mean(scores):.2f}")
+    summary_score_dict[pid] = np.mean(scores)
 
+import pandas as pd
+scores_df = pd.DataFrame({
+    "pid": score_dict.keys(),
+    "avg_score": [np.mean(scores) for scores in score_dict.values()]
+})
+
+# print the best 11 playes, no checking whether this is a valid team atm
+print(scores_df.sort_values(by="avg_score", ascending=False)[:11])
